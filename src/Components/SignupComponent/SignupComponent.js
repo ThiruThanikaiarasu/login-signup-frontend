28fb5bacf15413f26b320782a9bd1c2b497399b2
Thirunavukkarasu T <97720002+thiru042004@users.noreply.github.com>
2023-06-10 17:26:05 +0530

    Update. Included handler for form fields.

--- src/Components/SignupComponent/SignupComponent.js
+++ src/Components/SignupComponent/SignupComponent.js
@@ -1,9 +1,116 @@
 import React, { Component } from 'react'
 
 export class SignupComponent extends Component {
+
+    constructor(props) {
+      super(props)
+    
+      this.state = {
+        firstName : "",
+        lastName : "",
+        email : "",
+        password : "",
+      }
+    }
+
+    firstNameHandler = (event) =>{
+        this.setState({
+            firstName : event.target.value
+        })
+    }
+
+    lastNameHandler = (event) =>{
+        this.setState({
+            lastName : event.target.value
+        })
+    }
+
+    emailHandler = (event) =>{
+        this.setState({
+            email : event.target.value
+        })
+    }
+
+    passwordHandler = (event) =>{
+        this.setState({
+            password : event.target.value
+        })
+    }
+
+    formSubmitHandler = (event) =>{
+        event.preventDefault()
+        console.log(
+            this.state.firstName,
+            this.state.lastName,
+            this.state.email,
+            this.state.password
+        )
+
+    }
+
   render() {
+    const {firstName, lastName, email, password} = this.state
     return (
-      <div>SignupComponent</div>
+      <form onSubmit={this.formSubmitHandler} >
+        <h1>Sign UP</h1>
+
+        <div className='mb-3'>
+            <label>First Name</label>
+            <input 
+                type='text'
+                className='form-control'
+                placeholder='Enter your first name'
+                value={firstName}
+                onChange={this.firstNameHandler}
+                required
+            />
+        </div>
+
+        <div className='mb-3'>
+            <label>Last Name</label>
+            <input
+                type='text'
+                className='form-control'
+                placeholder='Enter your last name'
+                value={lastName}
+                onChange={this.lastNameHandler}
+                required
+            />
+        </div>
+
+        <div className='mb-3'>
+            <label>Email</label>
+            <input
+                type='email'
+                className='form-control'
+                placeholder='Enter your email address'
+                value={email}
+                onChange={this.emailHandler}
+                required
+            />
+        </div>
+
+        <div className='mb-3'>
+            <label>Password</label>
+            <input
+                type='password'
+                className='form-control'
+                placeholder='Enter your password'
+                value={password}
+                onChange={this.passwordHandler}
+                required
+            />
+        </div>
+
+        <div className='d-grid'>
+            <button type='submit' className='btn btn-primary' >Submit</button>
+        </div>
+
+        <p className='forgot-password text-right'>
+            Already registered, <a href='/login'>Sign in here?</a>
+        </p>
+
+      </form>
     )
   }
 }
